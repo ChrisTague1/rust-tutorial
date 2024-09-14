5d1d79902aec167f6adc00942951d6de194eac5e
Chris Tague <cstague6401@gmail.com>
2024-09-14 18:02:57 -0500

    finished section 17

--- sect17/blog/src/main.rs
+++ sect17/blog/src/main.rs
@@ -1,3 +1,23 @@
+use blog::Post;
+
+// Note: There is a better way of doing this in the book that I didn't want to write out.
+// It involves making it so that each state is it's own struct that returns the next struct in the
+// state. The API changes because you would have to do something like
+//
+// let post = Post::new();
+// let post = post.add_text
+// let post = post.request_review
+//
+// This is probably better anyway
 fn main() {
-    println!("Hello, world!");
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
